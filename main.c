47d723fa599ebe971a90daa0afcf70bb5899b34b
stiansenandreas <49850015+stiansenandreas@users.noreply.github.com>
2022-01-22 22:24:59 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-#include <string.h>
-#define STR_LEN 60
 
 int main() {
-    printf("Enter a word\n");
-    char word[STR_LEN+1];
-    scanf("%s", word);
-
-    int str_len = (int)strlen(word);
-    char alphabet[26]="abcdefghijklmnopqrstuvwxyz";
-    int letter_count[26]={0};
-
-    for (int i= 0; i < str_len; i++)
-    {
-        for (int j=0; j < 26; j++)
-        {
-            if (word[i] == alphabet[j])
-            {
-                letter_count[j]++;
-                printf("'%c' : %d\n", alphabet[j],letter_count[j]);
-            }
-        }
-    }
-
+    float value = 0;
+    float sum = 0;
+    float count = -1;
+    do {
+        printf("Enter a number: \n");
+        scanf("%g",&value);
+        printf("You entered %g\n",value);
+        sum += value;
+        count += 1;
+    } while (value != 0);
+    float average = sum/count;
+    printf("Count: %g\n",count);
+    printf("Sum: %g\n",sum);
+    printf("Average: %g\n",average);
     return 0;
 }
