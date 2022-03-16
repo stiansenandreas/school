6409b667db39c0b2fee406268298884fbe690076
stiansenandreas <49850015+stiansenandreas@users.noreply.github.com>
2022-03-16 10:58:15 +0100

    Add files via upload

---
+++ ikt103/assignment_5_2/student.h
@@ -0,0 +1,14 @@
+//
+// Created by stian on 04.02.2022.
+//
+
+#include <string>
+#include <iostream>
+
+class Student {
+public:
+    int id;
+    std::string name;
+    std::string email;
+    int year;
+};
